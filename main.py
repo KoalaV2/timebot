1b06b1d93e686d1a649ef8c8978e7377715d40f4
KoalaV2 <theo.johanson@gmail.com>
2021-04-07 02:14:01 +0200

    basic echo

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-import discord
+from discord.ext import commands
+import json
+
+with open("settings.json") as f:
+    try:configfile = json.load(f)
+    except:configfile = {}
+
+token = configfile['discordKey']
+print(token)
+prefix = "./"
+bot = commands.Bot(command_prefix=prefix)
 
 def main():
-    print("hello world")
+
+    @bot.event
+    async def on_ready():
+        print("Bot is online.")
+
+    @bot.command()
+    async def echo(ctx, *, content:str):
+        await ctx.send(content)
+
+    bot.run(token)  # Where 'TOKEN' is your bot token
 
 
 if __name__ == "__main__":
